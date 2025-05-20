40e5fad46adff0bfaa4bfaa56dc6b33123984982
kimkundad <kim.kundad@gmail.com>
2025-05-21 02:49:59 +0700

    kki

--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -10,50 +10,50 @@
     <meta name="apple-mobile-web-app-capable" content="yes">
     <meta name="author" content="heng1g">
     <meta name="keywords" content="">
-    <meta name="description" content="{{ get_facebook_detail() }}">
+    {{-- <meta name="description" content="{{ get_facebook_detail() }}"> --}}
     <title> @yield('title')</title>
     <link rel="icon" type="image/png" sizes="32x32" href="{{ url('img/favicon-32x32.png') }}" />
 
     <meta property="og:url"           content="https://heng1g.com/" />
     <meta property="og:type"          content="website" />
-    <meta property="og:title"         content="{{ get_title_facebook() }}" />
+    {{-- <meta property="og:title"         content="{{ get_title_facebook() }}" />
     <meta property="og:image"         content="{{ get_facebook_img() }}?v{{time()}}" />
-    <meta property="og:description"   content="{{ get_facebook_detail() }}" />
-    
+    <meta property="og:description"   content="{{ get_facebook_detail() }}" /> --}}
+
     @include('layouts.inc-style')
     @yield('stylesheet')
 
 </head>
 
 <body class="layout-row">
-    
+
 
         @if (Auth::guest())
         @else
         @php
-        $today = date("Y-m-d H:i"); 
-        $startdate = Auth::user()->birthday; 
+        $today = date("Y-m-d H:i");
+        $startdate = Auth::user()->birthday;
         $offset = strtotime("+1 day");
-        $enddate = date($startdate, $offset);    
+        $enddate = date($startdate, $offset);
         $today_date = new DateTime($today);
         $expiry_date = new DateTime($enddate);
         @endphp
-        @if($expiry_date < $today_date) 
+        @if($expiry_date < $today_date)
         <script>window.location = "{{ url('/logout') }}";</script>
         @endif
-   
+
 @endif
 
         @yield('content')
 
 
-    
+
 
     <!-- JavaScripts -->
     @include('layouts.inc-script')
     @yield('scripts')
 
-    
+
 </body>
 
 </html>
